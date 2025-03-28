34c99451447f020fc0374cc2b0f8fc0ad0ca4d2e
Reinier Cruz <reinier1011@yahoo.com>
2025-03-28 11:47:44 -0400

    creating summary files

---
+++ draft.go
@@ -0,0 +1,8 @@
+
+//Gets draft binary. Would be nice to have the binary in the repo. Might exceed github file limitations.
+
+//Wrap draft dockerfile creation
+
+//Wrap draft manifest creation
+
+//Future: Might require prior LLM analysis of file tree to determine entrypoint, and pass that in as input to draft
---
+++ llm-client.go
@@ -0,0 +1,4 @@
+//Initalizes an LLM client using https://github.com/tmc/langchaingo?tab=readme-ov-file
+// API key and endpoint are passed in as environment variables
+
+//Should be a package so that it can be imported by other files
