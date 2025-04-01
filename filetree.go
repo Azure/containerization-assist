b8f9b808654484b02797b92a475ce5705d166932
Tatsat Mishra <tamishra@microsoft.com>
2025-04-01 19:15:31 +1300

    Add readFileTree using dir walk.
    Signed-off-by: Tatsat Mishra <tamishra@microsoft.com>

--- filetree.go
+++ filetree.go
@@ -1,6 +1,66 @@
 package main
 
-func getFileTree()(string, error){
-	// TODO: actually read the file tree, respecting gitignore and create a string
-	return "./main.java",nil
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+
+	ignore "github.com/sabhiram/go-gitignore"
+)
+
+var defaultIgnores = []string{
+	"node_modules/",
+	"vendor/",
+	"go.mod",
+	"go.sum",
+	"bin/",
+	"obj/",
+	".git/",
+	".DS_Store",
+}
+
+func readFileTree(root string) (string, error) {
+	var buffer bytes.Buffer
+
+	// Load .gitignore if it exists
+	gitignorePath := filepath.Join(root, ".gitignore")
+	var gitIgnoreMatcher *ignore.GitIgnore
+	ignorePatterns := defaultIgnores
+
+	if _, err := os.Stat(gitignorePath); err == nil {
+		gitignoreContent, err := os.ReadFile(gitignorePath)
+		if err == nil {
+			ignorePatterns = append(ignorePatterns, string(gitignoreContent))
+		}
+	}
+
+	gitIgnoreMatcher = ignore.CompileIgnoreLines(ignorePatterns...)
+
+	// Walk the directory tree using Walk as suggested which is more efficient
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(root, path)
+		if err != nil {
+			return err
+		}
+
+		if gitIgnoreMatcher != nil && gitIgnoreMatcher.MatchesPath(relPath) {
+			if info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		buffer.WriteString(relPath + "\n")
+		return nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return buffer.String(), nil
 }
