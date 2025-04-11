0d5cb70025b964abcfca19d59dfd5ab1238cf45c
David Gamero <david340804@gmail.com>
2025-04-11 10:42:37 -0400

    blank dockerfile revert

--- container_copilot.go
+++ container_copilot.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -106,13 +105,10 @@ func (c *AzOpenAIClient) generate(targetDir string, registry string) error {
 	}
 
 	fmt.Printf("Using Dockerfile template: %s\n", draftTemplateName)
-	// write empty dockerfile to targetDir
-	fmt.Printf("writing empty dockerfile\n")
-	os.WriteFile(filepath.Join(targetDir, "Dockerfile"), []byte{}, fs.ModePerm)
-	// err = generateDockerfileWithDraft(draftTemplateName, targetDir)
-	// if err != nil {
-	// 	return fmt.Errorf("generating Dockerfile: %w", err)
-	// }
+	err = generateDockerfileWithDraft(draftTemplateName, targetDir)
+	if err != nil {
+		return fmt.Errorf("generating Dockerfile: %w", err)
+	}
 
 	fmt.Printf("Generating Kubernetes manifests in %s\n", targetDir)
 	registryAndImage := fmt.Sprintf("%s/%s", registry, "app")
