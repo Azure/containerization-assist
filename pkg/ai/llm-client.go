43dd30544d5bd659c4bb41626f58f2e3d57c2937
Reinier Cruz <reinier1011@yahoo.com>
2025-04-29 16:04:47 -0400

    increasing reasoning effor

--- pkg/ai/llm-client.go
+++ pkg/ai/llm-client.go
@@ -39,8 +39,8 @@ func (c *AzOpenAIClient) GetChatCompletion(promptText string) (string, error) {
 					Content: azopenai.NewChatRequestUserMessageContent(promptText),
 				},
 			},
-			Seed: to.Ptr(int64(42)),
-			TopP: to.Ptr(float32(0.00000001)),
+			Seed:            to.Ptr(int64(42)),
+			ReasoningEffort: to.Ptr(azopenai.ReasoningEffortValueHigh),
 		},
 		nil,
 	)
