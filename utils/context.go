bc781660305acfe531de0f88112cfa5d2ce77b84
Suneha Bose <suneha.bose@gmail.com>
2025-05-02 12:41:58 -0700

    refactor + tests

--- utils/context.go
+++ utils/context.go
@@ -10,14 +10,13 @@ import (
 // For example, LLM should produce a response that would include a new file inbetween two tags
 // for easy regex grabbing
 func GrabContentBetweenTags(content string, tag string) (string, error) {
-	// Create pattern for <TAG>content<TAG> format
-	pattern := fmt.Sprintf(`<%s>([\s\S]*?)</%s>`, tag, tag)
+	// Create pattern to  extract content between the last matching tag pair of <TAG>content<TAG> format
+	pattern := fmt.Sprintf("(?s).*<%s>([\\s\\S]*?)</%s>", regexp.QuoteMeta(tag), regexp.QuoteMeta(tag))
 	re := regexp.MustCompile(pattern)
 	matches := re.FindStringSubmatch(content)
-
-	if len(matches) > 1 {
-		return strings.TrimSpace(matches[1]), nil
+	if len(matches) < 2 {
+		return "", fmt.Errorf("content between tags <%s> not found", tag)
 	}
-
-	return "", fmt.Errorf("content between tags <%s> not found", tag)
+	
+	return strings.TrimSpace(matches[1]), nil
 }
