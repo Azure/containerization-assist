145456088d976b5a4713e4fa0b62ce6596521eb1
Ogheneobukome Ejaife <97336369+ejaifeobuks@users.noreply.github.com>
2025-06-05 19:21:44 -0400

    Update k8sclient.go

--- pkg/clients/k8sclient.go
+++ pkg/clients/k8sclient.go
@@ -100,7 +100,7 @@ func (c *Clients) DeployAndVerifySingleManifest(ctx context.Context, manifestPat
 	// Wait for pods to become healthy
 	podSuccess, podOutput := c.CheckPodStatus(ctx, namespace, labelSelector, time.Minute)
 	if !podSuccess {
-		logger.Debugf("    Retrieving logs for pods with label selector %s in namespace %s", labelSelector, namespa
+		logger.Debugf("    Retrieving logs for pods with label selector %s in namespace %s", labelSelector, namespace)
 		podLogs, err := GetDeploymentLogs(ctx, k8sAppName, namespace)
 		if err != nil {
 			logger.Errorf("Error retrieving deployment logs: %v\n", err)
