8a1b8e6a24148d9ab46eb15eb8dbb297459f7846
Reinier Cruz Carnero <reinier1011@yahoo.com>
2025-10-21 16:29:10 -0400

    cleanup

--- test/__support__/mocks/kubernetes-mock.ts
+++ test/__support__/mocks/kubernetes-mock.ts
@@ -1,9 +1,7 @@
 /**
- * Mock Kubernetes client for LLM integration tests
- * Provides minimal stubs to avoid import issues
+ * Simple Kubernetes client mock for LLM integration tests
  */
 
-// Mock the main kubernetes client exports
 export const KubeConfig = class {
   loadFromDefault() {}
   makeApiClient() {
@@ -11,31 +9,11 @@ export const KubeConfig = class {
   }
 };
 
-export const AppsV1Api = class {
-  constructor() {}
-};
+export const CoreV1Api = class {};
+export const AppsV1Api = class {};
 
-export const CoreV1Api = class {
-  constructor() {}
-};
-
-export const NetworkingV1Api = class {
-  constructor() {}
-};
-
-// Mock common kubernetes utilities
-export const loadYaml = () => ({});
-export const dumpYaml = () => '';
-
-// Default export for compatibility
 export default {
   KubeConfig,
-  AppsV1Api,
   CoreV1Api,
-  NetworkingV1Api,
-  loadYaml,
-  dumpYaml,
+  AppsV1Api,
 };
-
-// Mock any other exports that might be used
-export * from './kubernetes-mock';
