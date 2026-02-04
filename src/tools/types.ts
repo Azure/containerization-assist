7a79ef1e6ef8d0fd5334f637dabd3e32eb6c0ae6
Tony Chen (DevDiv) <chentony@microsoft.com>
2026-02-04 10:41:49 +0800

    Fix comment

--- src/tools/types.ts
+++ src/tools/types.ts
@@ -1,18 +1,3 @@
-export { analyzeRepoToolDefinition } from './analyze-repo/types';
-export { buildImageToolDefinition } from './build-image/types';
-export { fixDockerfileToolDefinition } from './fix-dockerfile/types';
-export { generateDockerfileToolDefinition } from './generate-dockerfile/types';
-export { generateK8sManifestsToolDefinition } from './generate-k8s-manifests/types';
-export { opsToolDefinition } from './ops/types';
-export { prepareClusterToolDefinition } from './prepare-cluster/types';
-export { pushImageToolDefinition } from './push-image/types';
-export { scanImageToolDefinition } from './scan-image/types';
-export { tagImageToolDefinition } from './tag-image/types';
-export { verifyDeployToolDefinition } from './verify-deploy/types';
-
-export { TOOL_NAME, type IToolDefinition } from './shared/toolDefinition';
-export type { ToolName } from './shared/toolDefinition';
-
 import { analyzeRepoToolDefinition } from './analyze-repo/types';
 import { buildImageToolDefinition } from './build-image/types';
 import { fixDockerfileToolDefinition } from './fix-dockerfile/types';
@@ -24,6 +9,23 @@ import { pushImageToolDefinition } from './push-image/types';
 import { scanImageToolDefinition } from './scan-image/types';
 import { tagImageToolDefinition } from './tag-image/types';
 import { verifyDeployToolDefinition } from './verify-deploy/types';
+
+export {
+  analyzeRepoToolDefinition,
+  buildImageToolDefinition,
+  fixDockerfileToolDefinition,
+  generateDockerfileToolDefinition,
+  generateK8sManifestsToolDefinition,
+  opsToolDefinition,
+  prepareClusterToolDefinition,
+  pushImageToolDefinition,
+  scanImageToolDefinition,
+  tagImageToolDefinition,
+  verifyDeployToolDefinition,
+};
+
+export { TOOL_NAME, type IToolDefinition } from './shared/toolDefinition';
+export type { ToolName } from './shared/toolDefinition';
 
 export const ALL_TOOL_DEFINITIONS = [
   analyzeRepoToolDefinition,
